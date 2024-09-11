59f3c1fbdbed0d5415779f1ab16451070b182f05
vakal.y <dugintseva.y@yandex.ru>
2024-09-11 16:17:30 +0300

    add footer content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,22 @@
 import styles from './Footer.module.scss';
+import { Link } from 'react-router-dom';
+import Logo from '../../ui/Logo/Logo';
+import telegram from '../../assets/icons/socialsTelegram.svg';
+import github from '../../assets/icons/socialsGit.svg';
 
 export default function Footer() {
     return (
-        <div className={styles.footer}><h1>Footer</h1></div>
+        <div className={styles.footer}>
+            <nav>
+                <ul>
+                    <li><Link>About us</Link></li>
+                    <li><Link to="/"><img src={telegram} alt="telegram" /></Link></li>
+                    <li><Link to="/"><img src={github} alt="github" /></Link></li>
+                    <li><p>Made by
+                        CMND+Fem collab</p></li>
+                    <li><Logo /></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
